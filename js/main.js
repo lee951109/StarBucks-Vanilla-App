4f1b090ed23ccbc91ceaf13802dcf918ab3473c8
lee951109 <sk985326@naver.com>
2022-04-28 21:49:00 +0900

    Add promotion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
-/*
-* 검색창 제어
-*/
+/**
+ * 검색창 제어
+ */
 // 검색창 요소(.search) 찾기.
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
@@ -20,19 +20,26 @@ searchInputEl.addEventListener('blur', function() {
   searchInputEl.setAttribute('placeholder', '');
 });
 
-// 뱃지 파트
+
+
+/**
+ * 페이지 스크롤에 따른 요소 제어
+ */
+// 페이지 스크롤에 영행을 받는 요소들을 검색
 const badeEL = document.querySelector('header .badges');
 
+//페이지에 스크롤 이벤트 추가
+//스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 준다)
 window.addEventListener('scroll', _.throttle(function() {
   if (window.scrollY > 500) {
-    // 배지 숨김
+    // Badge 숨김
     // gsap.to(요소, 지속시간, 옵션);
     gsap.to(badeEL, .4, {
       opacity : 0,
       display : 'none'
     });
   }else {
-    // 배지 보여줌
+    // Badge 보여줌
     gsap.to(badeEL, .6, {
       opacity : 1,
       display: 'block'
@@ -42,7 +49,13 @@ window.addEventListener('scroll', _.throttle(function() {
 // -.throttle(함수, 시간);
 
 
+
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
 const fadeEls = document.querySelectorAll('.visual .fade-in');
+// 나타날 요소들을 하나씩 반복해서 처리
 fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
     delay : (index + 1) * .6,
@@ -51,9 +64,45 @@ fadeEls.forEach(function (fadeEl, index) {
 });
 
 
+
+/**
+ * 슬라이드 요소 관리
+ */
 // new Swiper(선택자, 옵션)
 new Swiper('.notice-line .swiper', {
   direction: 'vertical', // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
 })
+new Swiper('.promotion .swiper', {
+  slidesPerView : 3, //한번에 보여줄 스라이드 개수
+  spaceBetween : 10, //슬라이드 사이 여백
+  centeredSlides : true, //1번 슬라이드가 가운데 보이기
+  loop : true,
+  autoplay : {
+    delay : 5000
+  },
+  pagination : {
+    el : '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable : true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation : {
+    prevEl : '.promotion .swiper-prev',
+    nextEl : '.promotion .swiper-next'
+  }
+});
+
+
+/**
+ * Promotion 슬라이드 토글 기능
+ */
+// 슬라이드 영역 요소
+const promotionEl = document.querySelector('.promotion');
+// 슬라이드 영역을 토글하는 버튼
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+// 슬라이드 영역 숨김 여부
+let isHideRomotion = false;
+
+promotionToggleBtn.addEventListener('click', function () {
+  isHideRomotion = !isHideRomotion
+});

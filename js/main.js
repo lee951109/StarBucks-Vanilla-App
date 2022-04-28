4eb9f0e3133e0f3e0e1b0369b1432f3edbc75552
lee951109 <sk985326@naver.com>
2022-04-28 20:40:13 +0900

    Add notice swiper

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 
-// 검색 파트 
+/*
+* 검색창 제어
+*/
+// 검색창 요소(.search) 찾기.
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
-
+// 검색창 요소를 클릭하면 실행.
 searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 });
-
+// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
 searchInputEl.addEventListener('focus', function() {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
 });
-
+// 검색창 요소 내부 실제 input 요서에서 포커스가 해제(블러)되면 실행.
 searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
@@ -46,3 +49,11 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity : 1
   });
 });
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
+})
